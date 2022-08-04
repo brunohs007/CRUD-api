f3b63d1e8db57311022f7ae709d8e874ef19281a
Bruno Holanda <64993036+brunohs007@users.noreply.github.com>
2022-08-04 19:14:46 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	Routers()
+}
+
+func Routers() {
+	InitDB()
+	defer db.Close()
+	log.Println("Starting the HTTP server on port 9080")
+	router := mux.NewRouter()
+	router.HandleFunc("/users",
+		GetPerfis).Methods("GET")
+	router.HandleFunc("/users",
+		CriarPerfil).Methods("POST")
+	router.HandleFunc("/users/{id}",
+		GetPerfil).Methods("GET")
+	router.HandleFunc("/users/{id}",
+		AtualizarPerfil).Methods("PUT")
+	router.HandleFunc("/users/{id}",
+		DeletarPerfil).Methods("DELETE")
+	http.ListenAndServe(":9080",
+		&CORSRouterDecorator{router})
+}
+
+//listar perfil
+func GetPerfis(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var users []Usuario
+	//ajustar senha (token)
+	result, err := db.Query("SELECT id, nome, email from users")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	for result.Next() {
+		var user Usuario
+		err := result.Scan(&user.ID, &user.Nome, &user.Email)
+		if err != nil {
+			panic(err.Error())
+		}
+		users = append(users, user)
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+//Create user
+func CriarPerfil(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	stmt, err := db.Prepare("INSERT INTO users(first_name," +
+		"last_name,email) VALUES(?,?,?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	nome := keyVal["nome"]
+	email := keyVal["email"]
+	_, err = stmt.Exec(nome, email)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "Novo usuario criado")
+}
+
+//listar perfil por ID
+func GetPerfil(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	result, err := db.Query("SELECT id, nome,email from users WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	var user Usuario
+	for result.Next() {
+		err := result.Scan(&user.ID, &user.Nome, &user.Email)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	json.NewEncoder(w).Encode(user)
+}
+
+//Atualizar usuario
+func AtualizarPerfil(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("UPDATE users SET nome = ?, email=? WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	nome := keyVal["nome"]
+	email := keyVal["email"]
+	_, err = stmt.Exec(nome, email,
+		params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "User with ID = %s was updated",
+		params["id"])
+}
+
+//Deleta usuario
+func DeletarPerfil(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("DELETE FROM users WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "User with ID = %s was deleted",
+		params["id"])
+}
+
+type Usuario struct {
+	ID    string `json:"id"`
+	Nome  string `json:"nome"`
+	Email string `json:"email"`
+	Data  string `json:"lastName"`
+}
+
+//Db configuration
+var db *sql.DB
+var err error
+
+func InitDB() {
+	db, err = sql.Open("mysql",
+		"root:@tcp(127.0.0.1:3306)/userdb")
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+type CORSRouterDecorator struct {
+	R *mux.Router
+}
+
+func (c *CORSRouterDecorator) ServeHTTP(rw http.ResponseWriter,
+	req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", origin)
+		rw.Header().Set("Access-Control-Allow-Methods",
+			"POST, GET, OPTIONS, PUT, DELETE")
+		rw.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Accept-Language,"+
+				" Content-Type, YourOwnHeader")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
+	}
+
+	c.R.ServeHTTP(rw, req)
+}
